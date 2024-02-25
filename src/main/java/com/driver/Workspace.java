c66ac1f786a8ee3d67190c2a42a70f98e754a29c
Gupta-deepak1906 <gdeepakg9997@gmail.com>
2024-02-25 11:21:29 +0530

    Add mail to 2

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -29,9 +29,30 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-        ArrayList<Meeting>curr = new ArrayList<>();
+        ArrayList<Meeting>m = new ArrayList<>();
+        LocalTime time_limit;
 
-        Collections.sort(curr, Comparator.comparing(Meeting::getStartTime));
+        Collections.sort(calendar,(m1,m2)->{
+            return m1.getEndTime().compareTo(m2.getEndTime());
+        });
+        //Initially select first meeting
+        Meeting meet = calendar.get(0);
+        m.add(meet);
+        //time_limit to check whether new meeting cn be conducted or not
+        time_limit = calendar.get(0).getEndTime();
+
+        //check for all meeting whether it can be selected or not
+        for(int i =0;i< calendar.size();i++){
+            if(calendar.get(i).getStartTime().compareTo(time_limit)>0){
+                //add selected meeting to arrylist
+                m.add(calendar.get(i));
+                //update time limit
+                time_limit=calendar.get(i).getEndTime();
+            }
+        }
+        return m.size();
+
+       /* Collections.sort(curr, Comparator.comparing(Meeting::getStartTime));
 
         LocalTime prevS = curr.get(0).getStartTime();
         LocalTime prevE = curr.get(0).getEndTime();
@@ -51,7 +72,21 @@ public class Workspace extends Gmail{
                 }
             }
         }
-        return cnt+1;
+        return cnt+1;*/
 
+    }
+    class myComparator implements Comparator<Meeting>{
+        @Override public int compare(Meeting m1,Meeting m2)
+        {
+            if(m1.getEndTime().compareTo(m2.getEndTime())<0){
+                //return -1 if second object is bigger than first
+                return -1;
+            }
+            else if(m1.getEndTime().compareTo(m2.getEndTime())>0){
+                //return 1 if second object is smaller than first
+                return 1;
+            }
+            return 0;
+        }
     }
 }
